a62388239dd11e3dfc8de600e6960e22af48692b
Ewa Olczak <ewa.olczak@gmail.com>
2022-10-25 21:52:17 +0200

    Add removal class active [ERROR UNSOLVED]

--- js/script.js
+++ js/script.js
@@ -7,19 +7,29 @@
 
 const titleClickHandler = function (event) {
 	console.log('Link was clicked!', event);
+
+	/* [ERROR: class 'active' is not removed] remove class 'active' from all article links */
+	const activeLinks = document.querySelectorAll('.titles a.active');
+
+	for (let activeLink of activeLinks) {
+		activeLink.classList.remove('active');
+	}
+
+	/* add class 'active' to the clicked link */
+
+	/* [ERROR: class 'active' is not removed] remove class 'active' from all articles */
+	const activeArticles = document.querySelectorAll('.posts article.active');
+
+	for (let activeArticle of activeArticles) {
+		activeArticle.classList.remove('active');
+	}
+
+	/* get 'href' attribute from the clicked link */
+
+	/* find the correct article using selecor (value of 'href' attribute) */
+
+	/* add class 'active' to the correct article */
 };
-
-/* remove class 'active' from all article links */
-
-/* add class 'active' to the clicked link */
-
-/* remove class 'active' from all articles */
-
-/* get 'href' attribute from the clicked link */
-
-/* find the correct article using selecor (value of 'href' attribute) */
-
-/* add class 'active' to the correct article */
 
 const links = document.querySelectorAll('.titles a');
 
