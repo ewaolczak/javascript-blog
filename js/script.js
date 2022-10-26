4cc01380f09bbeff0ab3df2d4e81f8ba68bca66e
Ewa Olczak <ewa.olczak@gmail.com>
2022-10-26 23:13:22 +0200

    Add adding class active, no errors

--- js/script.js
+++ js/script.js
@@ -10,16 +10,19 @@ const titleClickHandler = function (event) {
 	console.log('Link was clicked!');
 	console.log('event:', event);
 
-	/* remove class 'active' from all article links  */
+	/* [DONE] remove class 'active' from all article links  */
 	const activeLinks = document.querySelectorAll('.titles a.active');
 
 	for (let activeLink of activeLinks) {
 		activeLink.classList.remove('active');
 	}
 
-	/* add class 'active' to the clicked link */
+	/* [IN PROGRESS] add class 'active' to the clicked link */
+	const clickedElement = this;
+	console.log('clickedElement:', clickedElement);
+	clickedElement.classList.add('active');
 
-	/* remove class 'active' from all articles */
+	/* [DONE] remove class 'active' from all articles */
 	const activeArticles = document.querySelectorAll('.posts article.active');
 
 	for (let activeArticle of activeArticles) {
@@ -39,4 +42,3 @@ for (let link of links) {
 	// console.log(link);
 	link.addEventListener('click', titleClickHandler);
 }
-
